38dd38034c33711e36b2c9ecac6c5e9dbc72b8e9
shindeanita <shinde90anu@gmail.com>
2023-01-02 19:20:39 +0530

    Stubs notes file added.

---
+++ AccessorAndMutors/app/Http/Controllers/FlighController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FlighController extends Controller
+{
+    //
+    function index(){
+        return "This is index method";
+    }
+}
